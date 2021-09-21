fa9967413c65876de673ee800eea9b7660ef36b7
terrenceyee <terrence.yee@oxygencloud.com>
2021-09-21 13:30:35 -0700

    Update gateway_metadata_name handler

--- src/handler/v2/gateway_metadata_name.py
+++ src/handler/v2/gateway_metadata_name.py
@@ -12,8 +12,8 @@ def handle(environ):
 
     # PATH_INFO
     params = {
-        # URI /v2/metadata_content_name/<content.id>
-        'metadata.content.id': environ['PATH_INFO'][26:] if len(environ['PATH_INFO']) > 26 else None,
+        # URI /v2/gateway_metadata_name/<gateway.metadata.id>
+        'gateway.metadata.id': environ['PATH_INFO'][26:] if len(environ['PATH_INFO']) > 26 else None,
     }
 
     #
@@ -26,7 +26,7 @@ def handle(environ):
 
     delegate_func = '_{}{}'.format(
         environ['REQUEST_METHOD'].lower(),
-        '_metadata_content_name' if params['metadata.content.id'] else ''
+        '_gateway_metadata' if params['gateway.metadata.id'] else ''
     )
     if delegate_func in globals():
         return eval(delegate_func)(environ, params)
@@ -39,55 +39,55 @@ def handle(environ):
 
 
 # Rename file or folder.
-# PATCH /v2/metadata_content_name/<content.id>
+# PATCH /v2/gateway_metadata_name/<gateway.metadata.id>
 @util.handler.handle_unexpected_exception
 @util.handler.limit_usage
 @util.handler.handle_requests_exception
 @util.handler.load_access_token
 @util.handler.load_s3_config
 @util.handler.handle_s3_exception
-def _patch_metadata_content_name(environ, params):
-    assert params.get('metadata.content.id')
+def _patch_gateway_metadata(environ, params):
+    assert params.get('gateway.metadata.id')
 
     #
     # Load.
     #
 
     params.update({
-        'new.metadata.content.name': None,
-        'old.metadata.content.name': None,
+        'new.gateway.metadata.name': None,
+        'old.gateway.metadata.name': None,
     })
 
     # Load body.
     body = json.load(environ['wsgi.input'])
-    params['new.metadata.content.name'] = body.get('new.metadata.content.name')
-    params['old.metadata.content.name'] = body.get('old.metadata.content.name')
+    params['new.gateway.metadata.name'] = body.get('new.gateway.metadata.name')
+    params['old.gateway.metadata.name'] = body.get('old.gateway.metadata.name')
 
     #
     # Validate.
     #
 
     # Validate name.
-    if params['new.metadata.content.name'] is None:
+    if params['new.gateway.metadata.name'] is None:
         return {
             'code': '400',
-            'message': 'Missing new.metadata.content.name'
+            'message': 'Missing new.gateway.metadata.name'
         }
 
     #
     # Execute.
     #
 
-    renamed_content = controller.s3.rename(
+    result = controller.s3.rename(
         region=params['config.region'],
         host=params['config.host'],
         access_key=params['config.access.key'],
         access_key_secret=params['config.access.key.secret'],
         bucket=params['config.bucket'],
-        object_key=util.metadata_id.object_key(params['metadata.content.id']),
-        new_name=params['new.metadata.content.name'],
+        object_key=util.metadata_id.object_key(params['gateway.metadata.id']),
+        new_name=params['new.gateway.metadata.name'],
     )
-    if renamed_content is None:
+    if result is None:
         return {
             'code': '403',
             'message': 'Not allowed.'
@@ -96,5 +96,5 @@ def _patch_metadata_content_name(environ, params):
         'code': '200',
         'message': 'ok',
         'contentType': 'application/json',
-        'content': json.dumps(renamed_content)
+        'content': json.dumps(result)
     }
