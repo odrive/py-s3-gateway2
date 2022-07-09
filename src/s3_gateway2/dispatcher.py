b60b36ddc24acafc5f9fbcbce842b68cf7ccce59
terrenceyee <terrence.yee@oxygencloud.com>
2022-07-08 17:24:27 -0700

    Include upload segment size in auth response

--- src/s3_gateway2/dispatcher.py
+++ src/s3_gateway2/dispatcher.py
@@ -2,6 +2,7 @@ import s3_gateway2.util.s3
 import s3_gateway2.util.handler
 import s3_gateway2.controller.datastore
 import s3_gateway2.wsgi
+import s3_gateway2.handler.v2.gateway_auth
 
 
 def dispatch(environ, start_response):
@@ -14,3 +15,4 @@ def update_config(properties):
     s3_gateway2.controller.datastore.update_config(properties)
     s3_gateway2.util.handler.update_config(properties)
     s3_gateway2.util.s3.update_config(properties)
+    s3_gateway2.handler.v2.gateway_auth.update_config(properties)
